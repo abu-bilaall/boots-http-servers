4f2d78e6285f6dce96c2c2f4c5415cd4f70cd947
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-30 06:47:12 +0100

    feat: mount handlerValidateChirp, json middleware and error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import express from "express";
-import { handlerMetrics, handlerReadiness, handlerReset } from "./handlers.js";
-import { middlewareLogResponses, middlewareMetricsInc } from "./middlewares.js";
+import {
+  handlerMetrics,
+  handlerReadiness,
+  handlerReset,
+  handlerValidateChirp,
+} from "./handlers.js";
+import {
+  middlewareErrorHandling,
+  middlewareLogResponses,
+  middlewareMetricsInc,
+} from "./middlewares.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use(express.json());
+
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerValidateChirp);
+
 app.use("/admin/metrics", handlerMetrics);
-app.use("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
+
+app.use(middlewareErrorHandling);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
