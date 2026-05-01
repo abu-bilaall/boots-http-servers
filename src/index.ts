b6b19967d43ffe2bd0f911afcd1127a6fa7edefa
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-05-01 19:47:31 +0100

    feat: mount api/chirps and add auto migration support

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 import express from "express";
+
+import postgres from "postgres";
+import { config } from "./config.js";
+
 import {
+  handlerCreateChirp,
+  handlerDeleteUsers,
+  handlerGetAllChirps,
+  handlerGetChirp,
   handlerMetrics,
   handlerReadiness,
   handlerReset,
-  handlerValidateChirp,
+  handlerUsers,
 } from "./handlers.js";
+
 import {
   middlewareErrorHandling,
   middlewareLogResponses,
   middlewareMetricsInc,
+  middlewareValidateChirp,
 } from "./middlewares.js";
+
+process.loadEnvFile();
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
 
 const app = express();
 const PORT = 8080;
@@ -20,10 +36,13 @@ app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.get("/api/healthz", handlerReadiness);
-app.post("/api/validate_chirp", handlerValidateChirp);
+app.get("/api/chirps", handlerGetAllChirps);
+app.get("/api/chirps/:chirpId", handlerGetChirp);
+app.post("/api/chirps", handlerCreateChirp);
+app.post("/api/users", handlerUsers);
 
 app.use("/admin/metrics", handlerMetrics);
-app.post("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerDeleteUsers, handlerReset);
 
 app.use(middlewareErrorHandling);
 
