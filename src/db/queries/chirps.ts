48a80a3b87f549b9f6421deb3f58a025a0d7d63d
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-05-01 19:39:14 +0100

    feat: add query functions for chirps

---
+++ src/db/queries/chirps.ts
@@ -0,0 +1,20 @@
+import { eq } from "drizzle-orm";
+import { db } from "../index.js";
+import { chirps, type NewChirp } from "../schema.js";
+
+async function createChirp(chirp: NewChirp) {
+  const [result] = await db.insert(chirps).values(chirp).onConflictDoNothing().returning();
+  return result;
+}
+
+async function getAllChirps() {
+  const result = await db.select().from(chirps).orderBy(chirps.createdAt);
+  return result;
+}
+
+async function getChirp(id: string) {
+  const [result] = await db.select().from(chirps).where(eq(chirps.id, id));
+  return result;
+}
+
+export { createChirp, getAllChirps, getChirp };
