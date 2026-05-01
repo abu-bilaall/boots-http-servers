d6914e97e06198d91430793fb262b99f1508847f
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-05-01 19:46:22 +0100

    feat: add DBConfig and migration config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,43 @@
+import type { MigrationConfig } from "drizzle-orm/migrator";
+
+process.loadEnvFile();
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`${key} is missing in .env`);
+  }
+  return value;
+}
+
+const dbUrl = envOrThrow("DB_URL");
+const platform = envOrThrow("PLATFORM");
+
 type APIConfig = {
-  fileserverHits: number;
+  api: {
+    fileserverHits: number;
+    platform: string;
+  };
 };
 
-export const config: APIConfig = {
-  fileserverHits: 0,
+type DBConfig = {
+  db: {
+    url: string;
+    migrationConfig: MigrationConfig;
+  };
+};
+
+const migrationConfig: MigrationConfig = {
+  migrationsFolder: "./src/db/migrations",
+};
+
+export const config: DBConfig & APIConfig = {
+  db: {
+    url: dbUrl,
+    migrationConfig,
+  },
+  api: {
+    fileserverHits: 0,
+    platform: platform,
+  },
 };
