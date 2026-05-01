facaf57f53e92fa47f19d3cd7917f662bca08da1
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-05-01 19:44:38 +0100

    feat: add handlers for users and chirps

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,7 +2,10 @@ import { readFile } from "node:fs";
 import path from "node:path";
 import type { NextFunction, Request, Response } from "express";
 import { config } from "./config.js";
-import { BadRequestError } from "./customErrors.js";
+import { BadRequestError, ForbiddenError, NotFoundError } from "./customErrors.js";
+import { createChirp, getAllChirps, getChirp } from "./db/queries/chirps.js";
+import { createUser, deleteAllUsers } from "./db/queries/users.js";
+import type { NewChirp } from "./db/schema.js";
 
 function handlerReadiness(_req: Request, res: Response) {
   res.set("Content-Type", "text/plain");
@@ -19,7 +22,7 @@ function handlerMetrics(_req: Request, res: Response, next: NextFunction) {
         "Content-Type": "text/html",
         charset: "utf-8",
       });
-      res.send(data.replace("NUM", String(config.fileserverHits)));
+      res.send(data.replace("NUM", String(config.api.fileserverHits)));
     });
   } catch (error) {
     next(error);
@@ -27,7 +30,7 @@ function handlerMetrics(_req: Request, res: Response, next: NextFunction) {
 }
 
 function handlerReset(_req: Request, res: Response) {
-  config.fileserverHits = 0;
+  config.api.fileserverHits = 0;
   res.set({
     "Content-Type": "text/plain",
     charset: "utf-8",
@@ -36,29 +39,82 @@ function handlerReset(_req: Request, res: Response) {
   res.send(`Hits have been reset`);
 }
 
-function handlerValidateChirp(req: Request, res: Response, next: NextFunction) {
-  type BodyPayload = { body: string };
+type ChirpBodyPayload = { body: string; userId: string };
+function cleanChirp(parsedBody: ChirpBodyPayload) {
   const profane = ["kerfuffle", "sharbert", "fornax"];
 
+  if (parsedBody.body.length > 140) {
+    throw new BadRequestError("Chirp is too long. Max length is 140");
+  }
+
+  const regex = new RegExp(`\\b(${profane.join("|")})\\b`, "gi");
+
+  const cleanedBody = parsedBody.body.replace(regex, "****");
+  return { body: cleanedBody, userId: parsedBody.userId };
+}
+
+async function handlerCreateChirp(req: Request, res: Response, next: NextFunction) {
   try {
-    const parsedBody: BodyPayload = req.body;
-
-    if (parsedBody.body.length > 140) {
-      // return res.status(400).json({ error: "Chirp is too long" });
-      throw new BadRequestError("Chirp is too long. Max length is 140");
-    }
-
-    // Build regex like: /\b(kerfuffle|sharbert|fornax)\b/gi
-    const regex = new RegExp(`\\b(${profane.join("|")})\\b`, "gi");
-
-    const cleanedBody = parsedBody.body.replace(regex, "****");
-
-    return res.status(200).json({
-      cleanedBody,
-    });
+    const payload: NewChirp = cleanChirp(req.body);
+    const newChirp = await createChirp(payload);
+    res.status(201).json(newChirp);
   } catch (error) {
     next(error);
   }
 }
 
-export { handlerMetrics, handlerReadiness, handlerReset, handlerValidateChirp };
+async function handlerGetAllChirps(_req: Request, res: Response, next: NextFunction) {
+  try {
+    const chirps = await getAllChirps();
+    res.status(200).json(chirps);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function handlerGetChirp(req: Request, res: Response, next: NextFunction) {
+  try {
+    const chirpDetails = await getChirp(req.params.chirpId as string);
+    if (chirpDetails) {
+      res.status(200).json(chirpDetails);
+    } else {
+      throw new NotFoundError("Invalid ID. Chirp not found.");
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function handlerUsers(req: Request, res: Response, next: NextFunction) {
+  type BodyPayload = { email: string };
+
+  try {
+    const reqBody: BodyPayload = req.body;
+    const newUser = { email: reqBody.email };
+    const { email, id, ...newUserDetails } = await createUser(newUser);
+    return res.status(201).json({ email, id, newUserDetails });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function handlerDeleteUsers(_req: Request, res: Response, next: NextFunction) {
+  try {
+    if (config.api.platform !== "dev") throw new ForbiddenError("Environment is not 'dev.'");
+    await deleteAllUsers();
+    res.status(200).json({ message: "all users deleted" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export {
+  handlerCreateChirp,
+  handlerDeleteUsers,
+  handlerGetAllChirps,
+  handlerGetChirp,
+  handlerMetrics,
+  handlerReadiness,
+  handlerReset,
+  handlerUsers,
+};
