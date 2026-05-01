9b876ec989ed848a06cad4dd97b3d690c6db1d04
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-05-01 19:43:41 +0100

    feat: extend error middleware to support forbidden and not found errors

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,6 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { config } from "./config.js";
-import { BadRequestError } from "./customErrors.js";
+import { BadRequestError, ForbiddenError, NotFoundError } from "./customErrors.js";
 
 function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
   res.on("finish", () => {
@@ -13,13 +13,21 @@ function middlewareLogResponses(req: Request, res: Response, next: NextFunction)
 }
 
 function middlewareMetricsInc(_req: Request, _res: Response, next: NextFunction) {
-  config.fileserverHits++;
+  config.api.fileserverHits++;
   next();
 }
 
 function middlewareErrorHandling(err: Error, _req: Request, res: Response, _next: NextFunction) {
   if (err instanceof BadRequestError) {
     res.status(400).json({ error: err.message });
+  }
+
+  if (err instanceof ForbiddenError) {
+    res.status(403).json({ error: err.message });
+  }
+
+  if (err instanceof NotFoundError) {
+    res.status(404).json({ error: err.message });
   }
 
   console.log(err);
