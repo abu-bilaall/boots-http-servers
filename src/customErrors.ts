2157314856f7517695549148d0761c1429bc6814
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-05-02 15:58:04 +0100

    feat: add PasswordHashError

--- src/customErrors.ts
+++ src/customErrors.ts
@@ -2,5 +2,6 @@ class BadRequestError extends Error {}
 class UnauthorizedError extends Error {}
 class ForbiddenError extends Error {}
 class NotFoundError extends Error {}
+class PasswordHashError extends Error {}
 
-export { BadRequestError, ForbiddenError, NotFoundError, UnauthorizedError };
+export { BadRequestError, ForbiddenError, NotFoundError, PasswordHashError, UnauthorizedError };
